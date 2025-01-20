bfda915de5e1fbe63dc48c52a6794ba5d7f1022c
harveymcarter <harveymcarter@gmail.com>
2025-01-20 21:32:44 +0300

    Fix routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -16,23 +16,25 @@ import { LanguageProvider } from './contexts/LanguageContext';
 function App() {
   return (
     <LanguageProvider>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow pt-20">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/articles" element={<Articles />} />
-            <Route path="/articles/:slug" element={<ArticleDetail />} />
-            <Route path="/press" element={<PressKit />} />
-            <Route path="/speaking" element={<Speaking />} />
-            <Route path="/speaking/recording/:eventId" element={<EventRecording />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <HashRouter>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-grow pt-20">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/articles" element={<Articles />} />
+              <Route path="/articles/:slug" element={<ArticleDetail />} />
+              <Route path="/press" element={<PressKit />} />
+              <Route path="/speaking" element={<Speaking />} />
+              <Route path="/speaking/recording/:eventId" element={<EventRecording />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </HashRouter>
     </LanguageProvider>
   );
 }
